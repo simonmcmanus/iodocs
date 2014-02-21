dc77cfb3e01bd7346c107dba6212e7b0db5b4768
Simon McManus <mcmanus.simon@gmail.com>
2014-02-21 10:32:59 +0000

    sending json

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ function oauth2(req, res, next){
             var accessURL = apiConfig.oauth2.baseSite + apiConfig.oauth2.accessTokenURL;
             var basic_cred = apiKey + ':' + apiSecret;
             var encoded_basic = new Buffer(basic_cred).toString('base64');
- 
+
             var http_method = (apiConfig.oauth2.authorizationHeader == 'Y') ? "POST" : "GET";
             var header = (apiConfig.oauth2.authorizationHeader == 'Y') ? {'Authorization' : 'Basic ' + encoded_basic} : '';
             var fillerpost = query.stringify({grant_type : "client_credentials", client_id : apiKey, client_secret : apiSecret});
@@ -280,7 +280,7 @@ function oauth2(req, res, next){
             db.expire(key + ':baseURL', 1209600000);
 
             //client_credentials w/Authorization header
-            oa._request(http_method, accessURL, header, 
+            oa._request(http_method, accessURL, header,
                 fillerpost,
                 '', function(error, data, response) {
                 if (error) {
@@ -307,7 +307,7 @@ function oauth2(req, res, next){
                         db.set(key + ':refreshToken', oauth2refresh_token, redis.print);
                         db.expire(key + ':accessToken', 1209600000);
                         db.expire(key + ':refreshToken', 1209600000);
-                        
+
                         res.send({'refresh': callbackURL});
                     });
                 }
@@ -550,7 +550,8 @@ function processRequest(req, res, next) {
         };
 
     if (['POST','DELETE','PUT'].indexOf(httpMethod) !== -1) {
-        var requestBody = query.stringify(params);
+
+        var requestBody = JSON.stringify(params);
     }
 
     if (apiConfig.oauth) {
@@ -821,6 +822,7 @@ function processRequest(req, res, next) {
         if(options.headers === void 0){
             options.headers = {}
         }
+        console.log('CL', !options.headers['Content-Length'], requestBody)
         if (!options.headers['Content-Length']) {
             if (requestBody) {
                 options.headers['Content-Length'] = requestBody.length;
@@ -831,7 +833,7 @@ function processRequest(req, res, next) {
         }
 
         if (!options.headers['Content-Type'] && requestBody) {
-            options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+            options.headers['Content-Type'] = 'application/json';
         }
 
         if (config.debug) {
@@ -849,6 +851,7 @@ function processRequest(req, res, next) {
         }
 
         // API Call. response is the response from the API, res is the response we will send back to the user.
+        console.log('options are', options)
         var apiCall = doRequest(options, function(response) {
             response.setEncoding('utf-8');
 
@@ -896,7 +899,12 @@ function processRequest(req, res, next) {
         });
 
         if (requestBody) {
-            apiCall.end(requestBody, 'utf-8');
+            console.log('body is', typeof requestBody, requestBody)
+            apiCall.on('error', function() {
+                console.log('error', arguments)
+            });
+            apiCall.write(requestBody);
+            apiCall.end();
         }
         else {
             apiCall.end();
